d5ee5056f7107aff3bc86ba814d7bcc181893629
onirjar-commits <onirjar@gmail.com>
2026-02-05 12:44:56 +0000

    Add basic calculator menu implementation in Assignment 2

---
+++ assignment 2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int choice;
+    float a, b;
+
+    printf("Menu:\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+
+    printf("Enter your choice (1-4): ");
+    scanf("%d", &choice);
+}
