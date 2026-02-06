67f75581e395c1c848d20aaa782f0a802a37aa57
onirjar-commits <onirjar@gmail.com>
2026-02-06 06:13:33 +0000

    Add initial implementation for grading system to capture marks for 5 subjects

---
+++ assignment 8_Grading system.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+
+int main() {
+    int m1, m2, m3, m4, m5;
+    int total;
+    float percentage;
+
+    printf("Enter marks for 5 subjects: ");
+    scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+}
