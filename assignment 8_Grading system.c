086afca780000f920ec8cc9361000ae56cc9aacd
onirjar-commits <onirjar@gmail.com>
2026-02-06 06:20:09 +0000

    Implement grading logic to calculate total and percentage based on marks for 5 subjects

--- assignment 8_Grading system.c
+++ assignment 8_Grading system.c
@@ -7,4 +7,24 @@ int main() {
 
     printf("Enter marks for 5 subjects: ");
     scanf("%d %d %d %d %d", &m1, &m2, &m3, &m4, &m5);
+
+    if(m1< 40 || m2 < 40 || m3 < 40 || m4 < 40 || m5 < 40) {
+        printf("Grade: F\n");
+    } else {
+        total = m1 + m2 + m3 + m4 + m5;
+        percentage = (total / 500.0) * 100;
+
+        printf("Results: Pass \n");
+        printf("Total percentage: %.2f%%\n", percentage);
+
+        if(percentage >= 75) {
+            printf("Grade: Distinction\n");
+        } else if(percentage >= 60) {
+            printf("Grade: First Division\n");
+        } else if(percentage >= 50) {
+            printf("Grade: Second Division\n");
+        } else {
+            printf("Grade: Third Division\n");
+        }
+    }
 }
