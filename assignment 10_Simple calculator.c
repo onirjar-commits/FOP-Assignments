8ea058440b6b09a8ed3f934158fe879aff2abc55
onirjar-commits <onirjar@gmail.com>
2026-02-06 06:35:25 +0000

    Add options for multiplication, division, power, and factorial in the simple calculator

--- assignment 10_Simple calculator.c
+++ assignment 10_Simple calculator.c
@@ -9,6 +9,12 @@ int main() {
     printf("Simple Calculator\n");
     printf("1. Addition\n");
     printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("5. Power(x^y)\n");
+    printf("6. Factorial (x!)\n");
 
+    printf("Enter your choice (1-6): ");
+    scanf("%d", &choice);
 
 }
