f44aee16497c0e08284824e35cb17592235d0584
onirjar-commits <onirjar@gmail.com>
2026-02-06 06:34:12 +0000

    Add initial implementation of a simple calculator

---
+++ assignment 10_Simple calculator.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+int main() {
+    int choice;
+    int x, y, i;
+    float result;
+    long fact = 1;
+
+    printf("Simple Calculator\n");
+    printf("1. Addition\n");
+    printf("2. Subtraction\n");
+
+
+}
