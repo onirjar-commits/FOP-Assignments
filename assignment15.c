10508a82dde65722ca4736cff6318c4e44745aff
onirjar-commits <onirjar@gmail.com>
2026-04-02 11:10:27 +0530

    Update assignment15.c

--- assignment15.c
+++ assignment15.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int i;
+    int n,i;
 
+    printf("Enter the number of random numbers you want: ");
+    scanf("%d", &n);
+    srqnd(time(0));
 
-    for(i=1;i<=3;i++)
-    {
+    printf("Pseudo random numbers :");
+    for(i=1;i<=n;i++) {
         printf("%d\n",rand());
     }
 
